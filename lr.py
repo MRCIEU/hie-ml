90b4ecbb3861e62d975cc2f1d2a722593f1e7d73
Matt Lyon <ml18692@bristol.ac.uk>
2020-12-07 11:09:25 +0000

    added lr

---
+++ lr.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+import logging
+from sklearn.linear_model import LogisticRegression
+import argparse
+
+parser = argparse.ArgumentParser(description='Logistic regression model')
+parser.add_argument('--data', dest='data', required=True, help='Dataset to analyse')
+parser.add_argument('--outcome', dest='outcome', required=True, help='Outcome to analyse')
+parser.add_argument('--fmodel', dest='fmodel', required=True, help='Feature selection model')
+parser.add_argument('--nfeatures', dest='nfeatures', required=True, help='Number of features to include in model')
+args = parser.parse_args()
+
+# read in data
+train = pd.read_csv("data/{}{}_train.csv".format(args.data, args.outcome), index_col=0).astype('float32')
+test = pd.read_csv("data/{}{}_test.csv".format(args.data, args.outcome), index_col=0).astype('float32')
+
+# split outcome from predictors
+train_y = train.pop(args.outcome)
+test_y = test.pop(args.outcome)
+
+# select predictors
+features = pd.read_csv("data/{}{}.{}_features.csv".format(args.data, args.outcome, args.fmodel), index_col=0).astype('float32')
+if args.fmodel == "RFE":
+    predictors = features.sort_values(by='values', ascending=True).head(n=args.nfeatures)['feature']
+else:
+    features["values"] = features["values"].abs()
+    predictors = features.sort_values(by='values', ascending=False).head(n=args.nfeatures)['feature']
+
+# subset features
+train = train[predictors]
+test = test[predictors]
+
+# fit model
+clf = LogisticRegression(random_state=1234, penalty='none', max_iter=1e+8).fit(train, train_y)
+
+# test model
+y = clf.predict_proba(test)[:, 1]
+
+# write out results
+pd.DataFrame({"prob": y}).to_csv("data/{}{}_{}_n{}.".format(args.data, args.outcome, args.fmodel, args.nfeatures) + "_LR_prob.csv", index=test.index)
