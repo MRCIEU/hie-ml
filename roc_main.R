534c5b765bb06edd4a48797a0aef8c553c01667a
Matt Lyon <ml18692@bristol.ac.uk>
2021-03-11 12:03:01 +0000

    added fig2

--- roc_main.R
+++ roc_main.R
@@ -1,8 +1,3 @@
-# main roc plot
-
-# Elastic Net with Antenatal and Growth Factors (best performing combination under logistic regression)
-# and have a single figure comparing the ML methods on this (replacing figs 3-5, with just three sub-plots – one for each ML method)
-
 library("data.table")
 library("pROC")
 library("ggplot2")
@@ -12,23 +7,62 @@ library("ggpubr")
 library("readstata13")
 set.seed(123)
 
-# produce automl ROCs
-for (alg in c("LR", "RF", "NB", "NN")){
-    message(alg)
+get_rocs <- function(data, probs, model="LR", title=F){
+    plots <- list()
 
-    antenatal_growth <- get_rocs("antenatal_growth", probs, model=alg)
+    for (fmodel in c("ElasticNet")){
+        roc.dat <- list()
+        for (nfeatures in c(20, 40, 60)){
+            # create ROC
+            probs.tmp <- probs %>% filter(model==!!model & data==!!data & fmodel==!!fmodel & nfeatures==!!nfeatures)
+            roc.tmp <- roc(probs.tmp$bin, probs.tmp$prob, auc=TRUE, ci=TRUE)
+            roc.dat[[paste0(paste0("n", nfeatures), "\nAUC ", round(roc.tmp$ci[2],2), "\n(95CI ", round(roc.tmp$ci[1], 2), ", ", round(roc.tmp$ci[3], 2), ")")]] <- roc.tmp
+        }
 
-    fig2 <- ggarrange(antenatal[["ElasticNet"]], antenatal_growth[["ElasticNet"]], antenatal_intrapartum[["ElasticNet"]], nrow=1, ncol=3)
+        # create plot
+        p <- ggroc(roc.dat) +
+            theme_light() +
+            theme(legend.title=element_blank(), text = element_text(size=12), legend.key.height=unit(3,"line")) +
+            geom_segment(aes(x = 1, xend = 0, y = 0, yend = 1), color="darkgrey", linetype="dashed")
 
-    fig1 <- annotate_figure(fig1, top = text_grob("RFE"), fig.lab.size = 16)
-    fig2 <- annotate_figure(fig2, top = text_grob("ElasticNet"), fig.lab.size = 16)
-    fig3 <- annotate_figure(fig3, top = text_grob("Lasso"), fig.lab.size = 16)
-    fig4 <- annotate_figure(fig4, top = text_grob("SVC"), fig.lab.size = 16)
-    fig5 <- annotate_figure(fig5, top = text_grob("Tree"), fig.lab.size = 16)
+        if (title){
+            p <- p + ggtitle(fmodel)
+        }
+        
+        plots[[fmodel]] <- p
+    }
 
-    fig <- ggarrange(fig1, fig2, fig3, fig4, fig5, nrow=5, ncol=1)
-
-    png(paste0("automl-", alg, "-roc.png"), width=480*3, height=480*5)
-    print(fig)
-    dev.off()
+    return(plots)
 }
+
+# read in results
+probs <- data.frame()
+for (data in c("antenatal_growth")){
+    for (outcome in c("_hie")){
+        for (fmodel in c("ElasticNet")){
+            for (nfeatures in c(20, 40, 60)){
+                for (model in c("LR", "RF", "NB", "NN")){
+                    probs.tmp <- fread(paste0("data/", data, outcome, "_", fmodel, "_n", nfeatures, "_", model, "_prob.csv"), col.names=c("id", "prob", "bin"))
+                    probs.tmp$data <- data
+                    probs.tmp$outcome <- outcome
+                    probs.tmp$fmodel <- fmodel
+                    probs.tmp$nfeatures <- nfeatures
+                    probs.tmp$model <- model                    
+                    probs <- rbind(probs, probs.tmp)
+                }
+            }
+        }
+    }
+}
+
+# produce automl ROCs
+antenatal_growth_lr <- get_rocs("antenatal_growth", probs, model="LR")
+antenatal_growth_rf <- get_rocs("antenatal_growth", probs, model="RF")
+antenatal_growth_nb <- get_rocs("antenatal_growth", probs, model="NB")
+antenatal_growth_nn <- get_rocs("antenatal_growth", probs, model="NN")
+fig2 <- ggarrange(antenatal_growth_lr[["ElasticNet"]] + ggtitle("Logistic regression"), antenatal_growth_rf[["ElasticNet"]] + ggtitle("Random forest"), antenatal_growth_nb[["ElasticNet"]] + ggtitle("Naive Bayes"), antenatal_growth_nn[["ElasticNet"]] + ggtitle("Neural network"), nrow=2, ncol=2)
+
+# print to file
+png(paste0("automl-antenatal_growth-ES-roc.png"), width=480*2, height=480*2)
+print(fig2)
+dev.off()
