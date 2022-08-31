80d8187cb903f40ea618a235d9f1f9039bd42f65
Matt Lyon <ml18692@bristol.ac.uk>
2022-08-31 15:26:48 +0100

    added SMOTE and covariance filter

--- extract_features.py
+++ extract_features.py
@@ -3,13 +3,6 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from imblearn.over_sampling import SMOTE
 
-
-def split_data(df, x_cols, y_col):
-    x = df[x_cols + [y_col]]
-    x = x.dropna(axis='index')
-    y = x.pop(y_col)
-    return x, y 
-
 def standardize(df, features, means, stds):
     # create new data frame for standardised data
     result = pd.DataFrame()
@@ -23,7 +16,7 @@ def standardize(df, features, means, stds):
 
 # read in data from DO
 dat = pd.read_stata("data/1_2_3_4A._Done.dta")
-dat = dat.set_index('_id') 
+dat = dat.set_index('_id')
 
 # collect cat cols
 categorical = []
@@ -37,13 +30,7 @@ for col in dat.columns:
 for c in categorical:
     one_hot = pd.get_dummies(dat[c], prefix=c)
     dat = pd.concat([dat, one_hot], axis=1)
-    dat = dat.drop(c, axis=1) 
-
-# drop features with SD==0 in training data
-desc = dat.describe()
-stds = np.array(desc.T['std'])
-keep = desc.columns[stds!=0].tolist()
-dat = dat[keep]
+    dat = dat.drop(c, axis=1)
 
 # sep cols
 antenatal = []
@@ -77,12 +64,20 @@ for col in dat.columns:
         linear.append(col) 
 
 # split test and train
-train, test = train_test_split(dat, test_size=0.2, random_state=11)
+train, test = train_test_split(dat, test_size=0.5, random_state=11)
 
 # estimate mean and SD using training dataset
 desc = train[x].describe()
 means = np.array(desc.T['mean'])
 stds = np.array(desc.T['std'])
+
+# drop features with SD==0 in training data
+keep = desc.columns[stds!=0].tolist()
+train = train[keep]
+test = test[keep]
+antenatal = [x for x in antenatal if x in keep]
+antenatal_growth = [x for x in antenatal_growth if x in keep]
+antenatal_intrapartum = [x for x in antenatal_intrapartum if x in keep]
 
 # convert to Z score
 train = standardize(train, x, means, stds)
@@ -91,10 +86,26 @@ test = standardize(test, x, means, stds)
 for name, variable_list in {"antenatal" : antenatal, "antenatal_growth" : antenatal_growth, "antenatal_intrapartum" : antenatal_intrapartum}.items():
     for outcome in ['_hie']:
         print("Working on {} for {}".format(name, outcome))
-        
+
+        # drop features with high covariance
+        sorted_features = train[variable_list].isnull().sum().sort_values(ascending=True).index.to_list()
+        to_drop = set()
+        for feature in sorted_features:
+            if feature in to_drop:
+                continue
+            variable_list_tmp = list(filter(lambda x: (x not in to_drop), variable_list))
+            cor = train[variable_list_tmp].corrwith(train[feature]).abs()
+            to_drop.update(cor[cor > 0.95].index.to_list())
+        to_keep = list(filter(lambda x: (x not in to_drop), variable_list))
+
         # select variables for this analysis
-        train_x, train_y = split_data(train, variable_list, outcome)
-        test_x, test_y = split_data(test, variable_list, outcome)
+        train_x = train[to_keep + [outcome]]
+        train_x = train_x.dropna(axis='index')
+        train_y = train_x.pop(outcome)
+
+        test_x = test[to_keep + [outcome]]
+        test_x = test_x.dropna(axis='index')
+        test_y = test_x.pop(outcome)
 
         # oversample training dataset using SMOTE
         X_resampled, y_resampled = SMOTE().fit_resample(train_x, train_y)
