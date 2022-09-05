3f7130e265eb81aa40f046fed166e0b2f448027c
Matt Lyon <ml18692@bristol.ac.uk>
2022-09-05 11:45:54 +0100

    updated extract

--- extract_features.R
+++ extract_features.R
@@ -3,6 +3,18 @@ library("dplyr")
 library("mltools")
 set.seed(124)
 
+# Returns list of highly correlated features to drop
+to_drop <- function(train, features){
+    # subset features
+    train <- train %>% dplyr::select(features)
+    # sort features by missingness
+    features <- sapply(train_x, function(x) sum(is.na(x))) %>% sort %>% names
+    for (feature in features){
+        r <- cor(train[,feature, with=F], train[,-feature, with=F], use = "pairwise.complete.obs") %>% abs
+        r[r>0.95]
+    }
+}
+
 # read in data from DO
 dat <- read.dta13("data/1_2_3_4A._Done.dta")
 rownames(dat) <- dat[['_id']]
@@ -34,49 +46,38 @@ dat$train <- sample(c(T,F), nrow(dat), replace = T)
 train <- dat %>% dplyr::filter(train) %>% dplyr::select(-train)
 test <- dat %>% dplyr::filter(!train) %>% dplyr::select(-train)
 
-# estimate mean and SD using training dataset
-m <- apply(train, 2, function(x) mean(x, na.rm=T))
-s <- apply(train, 2, function(x) sd(x, na.rm=T))
+# split X and Y
+train_x <- train %>% dplyr::select(-'_hie')
+train_y <- train %>% dplyr::select('_hie')
+test_x <- test %>% dplyr::select(-'_hie')
+test_y <- test %>% dplyr::select('_hie')
 
-# Z-score normalisation
-train <- (train[-1] - m[-1]) / s[-1]
-test <- (test[-1] - m[-1]) / s[-1]
+# estimate mean and SD using training dataset
+m <- apply(train_x, 2, function(x) mean(x, na.rm=T))
+s <- apply(train_x, 2, function(x) sd(x, na.rm=T))
 
 # drop features with SD==0 in training data
-train <- train[,s>0,with=F]
-test <- test[,s>0,with=F]
+train_x <- train_x[,s>0,with=F]
+test_x <- test_x[,s>0,with=F]
 
+# Z-score normalisation of predictors
+train_x <- (train_x - m) / s
+test_x <- (test_x - m) / s
 
+# define feautre sets
+antenatal <- data.frame(id=names(train_x)) %>% 
+    dplyr::filter(!startsWith(id, "_")) %>% 
+    dplyr::filter(substr(id, 1, 1)=="a") %>% 
+    dplyr::pull(id)
 
+antenatal_growth <- data.frame(id=names(train_x)) %>% 
+    dplyr::filter(!startsWith(id, "_")) %>% 
+    dplyr::filter(substr(id, 1, 1)=="a" | substr(id, 1, 1)=="g") %>% 
+    dplyr::pull(id)
 
+antenatal_intrapartum <- data.frame(id=names(train_x)) %>% 
+    dplyr::filter(!startsWith(id, "_")) %>% 
+    dplyr::filter(substr(id, 1, 1)=="a" | substr(id, 1, 1)=="g" | substr(id, 1, 1)=="i") %>% 
+    dplyr::pull(id)
 
-for name, variable_list in {"antenatal" : antenatal, "antenatal_growth" : antenatal_growth, "antenatal_intrapartum" : antenatal_intrapartum}.items():
-    for outcome in ['_hie']:
-        print("Working on {} for {}".format(name, outcome))
-
-        # drop features with high covariance
-        sorted_features = train[variable_list].isnull().sum().sort_values(ascending=True).index.to_list()
-        to_drop = set()
-        for feature in sorted_features:
-            if feature in to_drop:
-                continue
-            variable_list_tmp = [x for x in variable_list if x not in to_drop and x != feature]
-            cor = train[variable_list_tmp].corrwith(train[feature]).abs()
-            to_drop.update(cor[cor > 0.95].index.to_list())
-        to_keep = [x for x in variable_list if x not in to_drop]
-
-        # select variables for this analysis
-        train_x = train[to_keep + [outcome]]
-        train_x = train_x.dropna(axis='index')
-        train_y = train_x.pop(outcome)
-
-        test_x = test[to_keep + [outcome]]
-        test_x = test_x.dropna(axis='index')
-        test_y = test_x.pop(outcome)
-
-        # oversample training dataset using SMOTE
-        X_resampled, y_resampled = SMOTE().fit_resample(train_x, train_y)
-
-        # write to csv
-        pd.concat([X_resampled, y_resampled], axis=1).to_csv("data/{}{}_train.csv".format(name, outcome), header=True)
-        pd.concat([test_x, test_y], axis=1).to_csv("data/{}{}_test.csv".format(name, outcome), header=True)
+# drop features with high covariance
